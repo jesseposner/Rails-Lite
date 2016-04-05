a32eff55760e7705eb0ff6f04bfbd89234df4f37
Jesse Posner <jesse.posner@gmail.com>
2016-04-05 14:46:50 -0700

    basic server, controller, and cookies

---
+++ lib/controller_base.rb
@@ -0,0 +1,59 @@
+require 'active_support'
+require 'active_support/core_ext'
+require 'erb'
+require 'byebug'
+require_relative './session'
+
+class ControllerBase
+  attr_reader :req, :res, :params
+
+  # Setup the controller
+  def initialize(req, res)
+    @req = req
+    @res = res
+  end
+
+  # Helper method to alias @already_built_response
+  def already_built_response?
+    @already_built_response
+  end
+
+  # Set the response status code and header
+  def redirect_to(url)
+    raise "double render" if @already_built_response
+    @res.header["location"] = url
+    @res.status = 302
+    @already_built_response = true
+    session.store_session(@res)
+  end
+
+  # Populate the response with content.
+  # Set the response's content type to the given type.
+  # Raise an error if the developer tries to double render.
+  def render_content(content, content_type)
+    raise "double render" if @already_built_response
+    @res['Content-Type'] = content_type
+    @res.write(content)
+    @already_built_response = true
+    session.store_session(@res)
+  end
+
+  # use ERB and binding to evaluate templates
+  # pass the rendered html to render_content
+  def render(template_name)
+    file_path = "views/#{self.class.to_s.underscore}/#{template_name}.html.erb"
+    file_content = File.read(file_path)
+    template = ERB.new(file_content)
+    content = template.result(binding)
+    render_content(content, "text/html")
+  end
+
+  # method exposing a `Session` object
+  def session
+    @session ||= Session.new(@req)
+  end
+
+  # use this with the router to call action_name (:index, :show, :create...)
+  def invoke_action(name)
+  end
+end
---
+++ lib/session.rb
@@ -0,0 +1,32 @@
+require 'json'
+require 'byebug'
+
+class Session
+  # find the cookie for this app
+  # deserialize the cookie into a hash
+  def initialize(req)
+    if req.cookies["_rails_lite_app"]
+      @cookie = JSON.parse(req.cookies["_rails_lite_app"])
+    else
+      @cookie = {}
+    end
+  end
+
+  def [](key)
+    @cookie[key]
+  end
+
+  def []=(key, val)
+    @cookie[key] = val
+  end
+
+  # serialize the hash into json and save in a cookie
+  # add to the responses cookies
+  def store_session(res)
+    json_cookie = JSON.generate(@cookie)
+    cookie_attributes = {}
+    cookie_attributes[:path] = "/"
+    cookie_attributes[:value] = json_cookie
+    res.set_cookie("_rails_lite_app", cookie_attributes)
+  end
+end
